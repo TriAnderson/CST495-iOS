2ae1d90408037d03ac38b6585353966c2c2ffe1c
Austin Thao <austinthao@Austins-Mac.local>
2017-12-03 19:34:14 -0800

    Create repo

---
+++ BetterLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BetterLife
+//
+//  Created by Austin Thao on 12/3/17.
+//  Copyright © 2017 CST495_BetterLIfe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
